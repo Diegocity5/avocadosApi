7bc46d8be314cde01464a586d4ce473be31aff57
Anonymosotec <icotecc@gmail.com>
2022-11-07 16:40:49 -0500

    Enriqueciendo informacion colocando nodo de montaje

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@
  * When you're ready to start on your site, clear the file. Consumiendo APIS es Genial ya que hay cientos de  Apis
  *!
  **/
-
-const url = "https://platzi-avo.vercel.app/api/avo";
+const baseUrl = "https://platzi-avo.vercel.app";
+// const url = "https://platzi-avo.vercel.app/api/avo";
+const nodeApp = document.querySelector('#app');
 
 //Web API
 //Conectarnos al server
 // Trabajando con PROMISE
-window.fetch(url)
+window.fetch(`${baseUrl}/api/avo`)
   //Procesar la respuesta, y convertirla en JSON
   .then((respuesta) => respuesta.json())
   //JSON -> Data -> Renderizar info browser
@@ -18,18 +19,20 @@ window.fetch(url)
     responseJson.data.forEach(item => {
         //crear un imagen
         const img = document.createElement('img');
+        img.src = `${baseUrl}${item.image}`;
         //crear un titulo
         const title = document.createElement('h2');
+        title.innerText = item.name;
         //crear un precio
         const price = document.createElement('div');
-
+        price.innerText = item.price;
         //card container
         const card = document.createElement('article');
         card.append(img, title, price);
         //Agregar al array javascript memoria
         allArticles.push(card);
     })
-    document.body.append(...allArticles);
+    nodeApp.append(...allArticles);
 });
 
 //Tarea: pudes logra este codigo tambien con asyn/await
